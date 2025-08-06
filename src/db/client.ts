693dea303fb677cab3375836f76bdd96e09a73d3
volen-t <vlknsrhntr@gmail.com>
2025-08-06 16:45:53 +0300

    Create client.ts

---
+++ src/db/client.ts
@@ -0,0 +1,14 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export const db = drizzle(pool);
