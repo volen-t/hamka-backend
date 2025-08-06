c871233863314097348de8c2d0219f85b36b8d7d
volen-t <vlknsrhntr@gmail.com>
2025-08-06 16:42:08 +0300

    Create schema.ts

---
+++ src/db/schema.ts
@@ -0,0 +1,18 @@
+import { pgTable, serial, text, integer } from "drizzle-orm/pg-core";
+
+export const shelves = pgTable("shelves", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+});
+
+export const yarns = pgTable("yarns", {
+  id: serial("id").primaryKey(),
+  brand: text("brand").notNull(),
+  code: text("code").notNull(),
+  color: text("color").notNull(),
+  thickness: text("thickness").notNull(),
+  quantity: integer("quantity").notNull(),
+  shelfId: integer("shelf_id")
+    .references(() => shelves.id)
+    .notNull(),
+});
